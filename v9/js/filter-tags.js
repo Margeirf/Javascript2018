1b9d677e368574d72b7d7884ff7461dfd856cf46
Aron DC <aron@merkiverk.net>
2018-04-06 09:50:59 +0000

    Add files via upload

--- v9/js/filter-tags.js
+++ v9/js/filter-tags.js
@@ -3,13 +3,56 @@
   var $imgs = $('#gallery img');                  // Store all images
   var $buttons = $('#buttons');                   // Store buttons element
   var tagged = {};                                // Create tagged object
-
-  var $imgs = $('#gallery img');          // Get the images
   var $search = $('#filter-search');      // Get the input element
   var cache = [];  
-  
+
+ var p1 = new Image();
+    p1.src = 'img/p1.jpg';
+    p1.tags = 'Animators, Illustrators'
+    p1.alt = 'Rabbit';
+var p2 = new Image();
+    p2.src = 'img/p2.jpg';
+    p2.tags = 'Photographers, Filmmakers';
+    p2.alt = 'Sea';
+var p3 = new Image();
+    p3.src = 'img/p3.jpg';
+    p3.tags = 'Photographers, Filmmakers';
+    p3.alt = 'Deer';
+var p4 = new Image();
+    p4.src = 'img/p4.jpg';
+    p4.tags = 'Designers';
+    p4.alt = 'New York Street Map';
+var p5 = new Image();
+    p5.src = 'img/p5.jpg';
+    p5.tags = 'Photographers, Filmmakers';
+    p5.alt = 'Trumpet Player';
+var p6 = new Image();
+    p6.src = 'img/p6.jpg',
+    p6.tags = 'Designers, Illustrators';
+    p6.alt = 'Typographic Study';
+var p7 = new Image();
+    p7.src = 'img/p7.jpg';
+    p7.tags = 'Photographers';
+    p7.alt = 'Bicycle Japan';
+var p8 = new Image();
+    p8.src = 'img/p8.jpg';
+    p8.tags = 'Designers';
+    p8.alt = 'Aqua Logo';
+var p9 = new Image();
+    p9.src = 'img/p9.jpg';
+    p9.tags = 'Animators, Illustrators';
+    p9.alt = 'Ghost';
+document.body.append(p1);
+    document.body.append(p2);
+    document.body.append(p3);
+    document.body.append(p4);
+    document.body.append(p5);
+    document.body.append(p6);
+    document.body.append(p7);
+    document.body.append(p8);
+    document.body.append(p9);
   function searchImg(){
-      $imgs.each(function() {                 // For each image
+      images.each(function() {                 // For each image
         cache.push({                          // Add an object to the cache array
           element: this,                      // This image
           text: this.alt.trim().toLowerCase() // Its alt text (lowercase trimmed)
